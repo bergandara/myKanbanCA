4d524c9b6df5df2515a7556bf45274a6460c8cb7
bergandara <gandaraber@gmail.com>
2023-05-02 00:26:17 +0100

    render.js created and basically edited

---
+++ render.js
@@ -0,0 +1,7 @@
+document.getElementById('kanban-board').addEventListener('click', () => {
+    // Code to open Kanban Board window
+});
+
+document.getElementById('pomodoro-timer').addEventListener('click', () => {
+    // Code to open Pomodoro Timer window
+});
