6a9399cc3cda52df917bd12206a6d5f9e56d0d3d
bergandara <gandaraber@gmail.com>
2023-05-03 23:37:07 +0100

    Changes done to autoamtically change between modes

--- PomodoroTimer/pomodoro.js
+++ PomodoroTimer/pomodoro.js
@@ -3,6 +3,7 @@ const timer = {
     shortBreak: 1,
     longBreak: 1,
     longBreakInterval: 4,
+    sessions: 0,
 }
 
 let interval;
@@ -39,6 +40,8 @@ function startTimer(){
     let { total } = timer.remainingTime;
     const endTime = Date.parse(new Date()) + total * 1000;
 
+    if (timer.mode === 'pomodoro') timer.sessions++;
+
     mainButton.dataset.action = 'stop';
     mainButton.textContent = 'stop';
     mainButton.classList.add('active');
@@ -50,6 +53,19 @@ function startTimer(){
         total = timer.remainingTime.total;
         if(total <=0) {
             clearInterval(interval);
+
+            switch (timer.mode){
+                case 'pomodoro':
+                    if(timer.sessions % timer.longBreakInterval === 0){
+                        switchMode('longBreak');
+                    }else {
+                        switchMode('shortBreak');
+                    }
+                    break;
+                default:
+                    switchMode('pomodoro');
+            }
+            startTimer();
         }
     }, 1000);
 }
@@ -95,9 +111,10 @@ function handleMode(event){
     if (!mode) return;
 
     switchMode(mode);
+    stopTimer();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     switchMode('pomodoro');
-    stopTimer();
+    
   });
